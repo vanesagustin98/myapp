b5f761cb7de3b6b6445ad74a1a369c257a5c40d0
Vanessa Gustin <vanesagustin98@hotmail.com>
2023-05-15 18:20:17 -0500

    commit2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,62 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('docente', function () {
+    return view('/docentes.docentes');
+});
+Route::get('estudiante', function () {
+    return view('/estudiantes.estudiantes');
+});
+Route::get('programa', function () {
+    return view('/programas.programas');
+});
+Route::get('facultad', function () {
+    return view('/facultades.facultad');
+});
+Route::get('/materia', function () {
+    return view('/materias.materia');
+});
+Route::get('/prueba/{parametro}', function ($parametro) {
+    return "Este es un parametro: {$parametro}";
+});
+Route::get('/prueba2/{parametro}', function ($parametro) {
+    return "Este es un parametro: {$parametro}";
+})->where('parametro','[0-9]+');
+Route::get('/prueba3/{parametro}', function ($parametro) {
+    return "Este es un parametro: {$parametro}";
+})->where('parametro','[a-y]+');
+Route::get('/prueba4/{parametro?}', function ($parametro=null) {
+    return "Este es un parametro: {$parametro}";
+})->where('parametro','[a-y]+');
+Route::get('/prueba5/{parametro1}/{parametro2?}', function ($parametro1, $parametro2=null) {
+    return "Este es un parametro: {$parametro1}";
+})->where('parametro1','[a-y]+');
+
+//------------------------ACTIVIDAD----------------------------------------------
+Route::get('actividad', function () {
+    return view('/actividad');
+});
+Route::get('actividad2', function () {
+    return view('/actividad.actividad');
+});
+Route::get('actividad3', function () {
+    return view('/actividad.refundido.actividad');
+});
+Route::get('/actividad1/{parametro?}', function ($parametro) {
+    return "Este es un parametro: {$parametro}";
+});
+Route::get('/actividad2/{parametro1}/{parametro2}', function ($parametro1, $parametro2) {
+    return "Este es un parametro: {$parametro1} {$parametro2}";
+});
+
+Route::get('/actividad3/{parametro1}/{parametro2?}', function ($parametro1, $parametro2=null) {
+    return "Este es un parametro: {$parametro1} {$parametro2}";
+});
+Route::get('/actividad4/{parametro}', function ($parametro) {
+    return "Este es un parametro: {$parametro}";
+})->where('parametro','[h,z,p]|[h,z,p]+[a-z]+');
+Route::get('/actividad5/{parametro}', function ($parametro) {
+    return "Este es un parametro: {$parametro}";
+})->where('parametro','[5,7]');
+
+
